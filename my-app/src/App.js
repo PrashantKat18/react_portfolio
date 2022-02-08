4706fa39c3c053c5176a0a5094b08c83e4ab2e3e
PrashantKat18 <prashantkatiyar10796@gmail.com>
2022-02-09 00:33:42 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hi Prashant </p>
+    </div>
+  );
+}
+
+export default App;
